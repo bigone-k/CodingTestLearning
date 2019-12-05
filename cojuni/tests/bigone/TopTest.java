9ad4e0c821aa8d55ced5fac961a02d277f4f56b1
bigone <trium10@gmail.com>
2019-12-05 16:21:17 +0900

    #issue1 Solution_bigone

---
+++ cojuni/tests/bigone/TopTest.java
@@ -0,0 +1,25 @@
+package bigone;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class TopTest {
+    Top top;
+    int[] heights;
+    int[] result;
+
+    @Before
+    public void setUp() {
+        top = new Top();
+        heights = new int[] {3,9,9,3,5,7,2};
+        result = new int[] {0,0,0,3,3,3,6};
+    }
+
+    @Test
+    public void runSolution() {
+        assertThat(top.solution(heights), is(result));
+    }
+}
