f09cafa0396adf4825abb33f21ada9478529e4dc
trium10 <trium10@gmail.com>
2020-05-24 19:34:39 +0900

    순열, 조합 문제 해결 중

---
+++ cojuni/src/bigone/programmers/TargetNumber.java
@@ -0,0 +1,29 @@
+package bigone.programmers;
+
+public class TargetNumber {
+    public int solution(int[] numbers, int target) {
+        return DFS(numbers, target, 0, 0);
+    }
+
+    public int DFS(int[] numbers, int target, int index, int num) {
+        if (index == numbers.length)
+        {
+            return num == target ? 1 : 0;
+        }
+        else
+        {
+            return DFS(numbers, target, index + 1, num + numbers[index]) +
+                    DFS(numbers, target, index + 1, num - numbers[index]);
+        }
+    }
+
+    public static void main(String arg[]) {
+        int[] arr = new int[]{
+                30,14,15,16,4
+        };
+
+        int target = 19;
+
+        System.out.println("result : " + new TargetNumber().solution(arr, target));
+    }
+}
